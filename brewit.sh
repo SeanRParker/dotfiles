ae8c9b11c715781d5c017fdb144490665f404575
Sean <SeanRParker@users.noreply.github.com>
2019-05-30 10:36:49 -0400

    Create brewit.sh
    script for installing xCode, homebrew and various apps.

---
+++ brewit.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+# XCode CLI
+xcode-select --install
+
+# Install homebrew
+/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+
+#spectacle
+brew cask install spectacle
+
+# postgres
+brew install postgres
+
+# fira code
+brew cask install font-fira-code
+
+# iterm2
+brew cask install iterm2
+
+# vscode
+brew cask install visual-studio-code
+
+# atom
+brew cask install atom
+
+# brave
+brew cask install brave-browser
+
+# firefox
+brew cask install firefox
+
+# firefoxDE
+brew cask install homebrew/cask-versions/firefox-developer-edition
+
+# spotify
+brew cask install spotify
