df59dc0deaeb279088a38f9135705917109c9ca5
SeanRParker <seanrichardparker@gmail.com>
2019-06-08 07:10:25 -0600

    move files to corresponding folders

---
+++ scripts/prefs_files.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# Bash Profile
+cp settings/.bash_profile
+
+#iTerm2 preferences
+cp -f settings/com.googlecode.iterm2.plist /Library/Preferences/
---
+++ setup.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+xcode-select --install
+
+# install homebrew and preferred apps (Spectacle, iTerm2, VS-Code) and settings (Git Aware, Fira Codefont).
+sh scripts/brewit.sh
+
+# Git Aware
+sh scripts/git_aware.sh
+
+#ASDF - ruby, node, and others in one package manager
+sh scripts/asdf.sh
+
+# moving bash_profile and iterm preferences to local system
+sh scripts/prefs_files.sh
+
+# SSH key
+sh scripts/ssh-setup.sh
+
