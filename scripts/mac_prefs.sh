2c62bea1b35adee9de3d8c8182d8741d4d988a61
SeanRParker <seanrichardparker@gmail.com>
2019-06-14 09:47:58 -0400

    include commented command to reenable dark mode

--- scripts/mac_prefs.sh
+++ scripts/mac_prefs.sh
@@ -9,4 +9,8 @@ defaults write com.apple.screencapture location ~/ScreenGrabs/
 # Dark menubar and dock with light windows
 defaults write -g NSRequiresAquaSystemAppearance -bool Yes
 
+# To disable:
+#  defaults write -g NSRequiresAquaSystemAppearance -bool No
+
+
 echo "Select 'Dark' mode in System Preferences >> General"
